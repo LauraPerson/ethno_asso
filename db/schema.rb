0cfb121767a22b0b4921bab745a331de0ba4310e
laura person <lauraperson29800@gmail.com>
2022-05-12 00:12:53 +0200

    production environement

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_08_195040) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -25,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_08_195040) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_ressources_on_user_id"
   end
 
